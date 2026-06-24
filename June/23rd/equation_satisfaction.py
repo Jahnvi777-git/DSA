32cf1d06f0b1a64bd3ba0b2169f34329eb1c06f4
Jahnvi777-git <148742397+Jahnvi777-git@users.noreply.github.com>
2026-06-24 14:42:31 +0530

    Create equation_satisfaction.py

---
+++ June/23rd/equation_satisfaction.py
@@ -0,0 +1,29 @@
+# link: https://leetcode.com/problems/find-the-town-judge/description/?envType=problem-list-v2&envId=d5qblqpi
+class UnionFind:
+    def __init__(self):
+        self.parent = list(range(26))
+    
+    def find(self, x):
+        if x!= self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if x!= y:
+            self.parent[x] = y
+            return True
+        return False
+
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        uf = UnionFind()
+        for a, e, _, b in equations:
+            if e == '=':
+                uf.union(ord(a) - ord('a'), ord(b) - ord('a'))
+        for a, e, _, b in equations:
+            if e == '!':
+                if uf.find(ord(a) - ord('a')) == uf.find(ord(b) - ord('a')):
+                    return False
+        return True
